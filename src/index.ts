b8709a004220c09e20a8cfa84708479153dc9781
IvanovJenyIT <jenyit@ya.ru>
2023-04-11 16:56:47 +0200

    change commite

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@
 import express, {Request, Response}  from'express';
 const app = express();
 
+const port = process.env.PORT || 5000
+
 app.get('/', (req: Request, res: Response) => {
   let helloMess = 'Hello Worlds6!';
   res.send(helloMess);
 });
 
-app.listen(4000, () => {
-  console.log('Server listening on port 4rr000');
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
 });
